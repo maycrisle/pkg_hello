d50af12d414a2ab5074c003e2df41aae8e9b4fe9
maycrisle <980902713@qq.com>
2022-10-26 23:03:58 +0800

    catkin test hello src files

---
+++ src/pkg_hello_node.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int number;
+    cout << "please enter a number to get double: " << endl;
+    cin >> number;
+    int answer = 2 * number;
+    cout << "the double of " << number << ", is: " << answer << endl;
+    cout << "Nice to meet you, too. Bye for now!" << endl;
+
+    return 0;
+}
